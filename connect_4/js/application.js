6426b393b857a4de8705ee8a239ac20c191a2fd0
Angela Field & jeremymcintyre <pair+angelafield.jeremymcintyre@devbootcamp.com>
2015-01-22 16:29:15 -0800

    add pseudocode

--- connect_4/js/application.js
+++ connect_4/js/application.js
@@ -1,5 +1,18 @@
+// on ready
 $(document).ready(function() {
+  // listen for click on TDs
+  var game = new Game();
   $('td').on('click', function() {
-    $(this).addClass('red')
-  })
+    // assign column to the td data-col
+    var column = $(this).attr('data-col');
+    // ask model if valid move
+    // aside: model will decide if it's valid
+    // if it is, model will push new red or black piece to the column,
+    // depending on the turn cycle
+    // will return color, data-row if valid
+    if (game.validMove(column)) {
+      game.insertToken
+    }
+
+  }).
 });
