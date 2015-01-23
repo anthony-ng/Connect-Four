a805fda494a4882b397401a452aee53239e339d0
Anthony Ng & Victoria Chuang <pair+anthony-ng.victoriachuang@devbootcamp.com>
2015-01-22 16:25:35 -0800

    controller outline

--- model.js
+++ model.js
@@ -1,11 +1,14 @@
-var Game = {
+function Game() {
   // holds whose turn => undefined variable, will be assigned red or black
-  var whoseTurn? = "red", // will indicate "red" or "black"
+  var whoseTurn? = "red"; // will indicate "red" or "black"
   // hold board representation => nested array, each array is a column
   // max length for each column is 6
-  var board = [[], [], [], [], [], [], []],
-  var hasWinner? = false,
+  var board = [[], [], [], [], [], [], []];
+  var hasWinner? = false;
 }
+
+board = [["red","black"], [], [] ,....]
+
 
 // check win
 Game.prototype.checkWin = function() {
@@ -16,7 +19,7 @@ Game.prototype.checkWin = function() {
   // check diagonal
 
 
-// if someone has won, hasWinner? = true, winner will be value of whoseTurn?
+  // if someone has won, hasWinner? = true, winner will be value of whoseTurn?
 
 }
 
@@ -24,40 +27,49 @@ Game.prototype.checkWin = function() {
 // if array length is less than 6, return true
 // function will be called on individual column array
 // this is column
-Game.prototype.validMove? = function() {
-  if (this.length < 6) {
+Game.prototype.validMove? = function(columnIndex) {
+  if (this.board[columnIndex].length < 6) {
     return true;
+  } else {
+    console.log("Pick another column!");
   }
 }
 
 // if validMove?, push whoseTurn? onto selected array
 // this is column
-Game.prototype.insertToken = function() {
-  if (this.validMove?) {
-    this.push(whoseTurn?)
-  }
+Game.prototype.insertToken = function(columnIndex) {
+  this.board[columnIndex].push(whoseTurn?);
 }
 
 // at the end of each turn, switch whoseTurn?
 
 // representation of game:
 
+
 while (hasWinner? === false) {
   if (whoseTurn? === "red") {
+    // 1st: check win
     checkWin();
-    // player select a column
-    validMove?();
-    // if (validMove?()) {
+    // 2nd: player select a column
+    if(validMove?) {
       insertToken();
-    // }
-      whoseTurn? = "black" // switch players
+    }
+
+    whoseTurn? = "black"; // switch players
   } else if (whoseTurn? === "black") {
-      checkWin();
-    // player select a column
-    validMove?();
-    if (validMove?()) {
+    checkWin();
+    // 2nd: player select a column
+    if(validMove?) {
       insertToken();
     }
-      whoseTurn? = "red" // switch players
-    }
+
+    whoseTurn? = "red"; // switch players
   }
+}
+
+
+// event listener - click on button to insert token
+// button will reference specific column
+
+
+
