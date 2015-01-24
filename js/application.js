c2bf39581842cd7ffa40188744fc73864ee28f97
jeremymcintyre <jmcintyre.dev@gmail.com>
2015-01-23 16:46:46 -0800

    Game should render on multiple devices as long as they share the same ID

--- js/application.js
+++ js/application.js
@@ -3,12 +3,27 @@
 $(document).ready(function() {
   var game = new Game();
   var myFirebaseRef = new Firebase("blazing-torch-1243.firebaseIO.com/" + game.id);
+  var myFirebaseMovesRef = new Firebase("blazing-torch-1243.firebaseIO.com/" + game.id + '/moves');
+
   myFirebaseRef.set({whoseTurn: "red", moves: game.moves});
 
   myFirebaseRef.on("value", function(snapshot){
       game.whoseTurn = snapshot.val().whoseTurn;
     });
 
+  myFirebaseMovesRef.on("value", function(snapshot){
+    // console.log(snapshot);
+    var row = snapshot.order;
+  });
+
+  myFirebaseMovesRef.on('child_added', function(snapshot) {
+    var moveObj = snapshot.val();
+    // console.log(moveObj.color);
+    // console.log(moveObj.col);
+    // console.log(moveObj.row);
+    $('*[data-row="' + moveObj.row + '"] *[data-col="' + moveObj.col + '"]').addClass(moveObj.color);
+
+  });
 
   $('td').on('click', function() {
   var column = $(this).attr('data-col');
@@ -22,7 +37,8 @@ $(document).ready(function() {
     var obj = game.insertToken(column);
 
     myFirebaseRef.update({moves: game.moves});
-    $('*[data-row="' + obj.row + '"] *[data-col="' + column + '"]').addClass(obj.color);
+
+
       game.checkHorizontal();
       game.checkVertical();
       game.checkDiagonal();
