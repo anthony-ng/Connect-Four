a8604ec7ea074eb5b83fecab4478e68f2488d01b
jeremymcintyre <jmcintyre.dev@gmail.com>
2015-01-23 16:00:59 -0800

    remove firebase listener from click listener; add gameId to namespace different games; add game.moves to track row, col, and color of each move in a game; NEXT need to udpate view on all computers connect to the same firebase route

--- js/application.js
+++ js/application.js
@@ -1,11 +1,15 @@
 // on ready
 
 $(document).ready(function() {
-  var myFirebaseRef = new Firebase("blazing-torch-1243.firebaseIO.com");
   var game = new Game();
-  myFirebaseRef.set({
-    whoseTurn: "red"
-  });
+  var myFirebaseRef = new Firebase("blazing-torch-1243.firebaseIO.com/" + game.id);
+  myFirebaseRef.set({whoseTurn: "red", moves: game.moves});
+
+  myFirebaseRef.on("value", function(snapshot){
+      game.whoseTurn = snapshot.val().whoseTurn;
+    });
+
+
   $('td').on('click', function() {
   var column = $(this).attr('data-col');
   if (game.validMove(column)) {
@@ -14,10 +18,10 @@ $(document).ready(function() {
     } else if (game.whoseTurn === "black") {
       myFirebaseRef.set({whoseTurn: "red" });
     }
-    myFirebaseRef.on("value", function(snapshot){
-      game.whoseTurn = snapshot.val().whoseTurn;
-    });
+
     var obj = game.insertToken(column);
+
+    myFirebaseRef.update({moves: game.moves});
     $('*[data-row="' + obj.row + '"] *[data-col="' + column + '"]').addClass(obj.color);
       game.checkHorizontal();
       game.checkVertical();
